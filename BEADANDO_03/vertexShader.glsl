e59e296f63831db40e081f6d4c5ef715b4b4c3bd
Máté Zagyva <150829347+Z-M4T3@users.noreply.github.com>
2026-05-08 12:58:48 +0200

    Refactor vertex shader to include texture coordinates

--- BEADANDO_03/vertexShader.glsl
+++ BEADANDO_03/vertexShader.glsl
@@ -1,18 +1,22 @@
-#version 330
+#version 330 core
 
-layout (location = 0) in vec3   aPosition;
-layout (location = 1) in vec3   aNormal;
+layout (location = 0) in vec3 aPosition;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoord;
 
-out vec3                         varyingFragmentPosition;
-out vec3                         varyingNormal;
+out vec3 varyingFragmentPosition;
+out vec3 varyingNormal;
+out vec2 varyingTextureCoord;
 
-uniform mat4                     matModel;
-uniform mat4                     matView;
-uniform mat4                     matProjection;
-uniform mat3                     inverseTransposeMatrix;
+uniform mat4 matModel;
+uniform mat4 matView;
+uniform mat4 matProjection;
+uniform mat3 inverseTransposeMatrix;
 
 void main(void) {
     varyingFragmentPosition = vec3(matModel * vec4(aPosition, 1.0));
-    varyingNormal           = inverseTransposeMatrix * aNormal;
-    gl_Position             = matProjection * matView * vec4(varyingFragmentPosition, 1.0);
+    varyingNormal = inverseTransposeMatrix * aNormal;
+    varyingTextureCoord = aTexCoord;
+    
+    gl_Position = matProjection * matView * vec4(varyingFragmentPosition, 1.0);
 }
