f0045af4e69ffd21f3feb6a8b921c0469d9070e7
Máté Zagyva <150829347+Z-M4T3@users.noreply.github.com>
2026-05-09 17:50:52 +0200

    Refactor fragment shader main function
    Refactor main function structure and improve readability.

--- BEADANDO_03/fragmentShader.glsl
+++ BEADANDO_03/fragmentShader.glsl
@@ -15,25 +15,32 @@ uniform sampler2D textureSampler;
 
 const vec3 ambientColor = vec3(1.0, 1.0, 1.0);
 const float ambientStrength = 0.2;
-const vec3 diffuseLightColor = vec3(1.0, 0.9, 0.5); 
+const vec3 diffuseLightColor = vec3(1.0, 0.9, 0.5);
 
-void main(void) {
-    if (useTexture) {
-        // Nap rajzolása textúrával
-        outColor = texture(textureSampler, varyingTextureCoord);
-    } else {
-        vec3 ambient = ambientStrength * ambientColor;
-        
-        if (useLighting) {
-            vec3 norm = normalize(varyingNormal);
-            vec3 lightDir = normalize(lightPosition - varyingFragmentPosition);
-            float diff = max(dot(norm, lightDir), 0.0);
-            vec3 diffuse = diff * diffuseLightColor;
-            
-            // Itt adjuk át a C++ kódból kapott fehér színt (objectColor)
-            outColor = vec4((ambient + diffuse) * objectColor, 1.0);
-        } else {
-            outColor = vec4(ambient * objectColor, 1.0);
-        }
+void main(void)
+{
+  if (useTexture)
+  {
+    // Nap rajzolas es textura
+    outColor = texture(textureSampler, varyingTextureCoord);
+  }
+  else
+  {
+    vec3 ambient = ambientStrength * ambientColor;
+
+    if (useLighting)
+    {
+      vec3 norm = normalize(varyingNormal);
+      vec3 lightDir = normalize(lightPosition - varyingFragmentPosition);
+      float diff = max(dot(norm, lightDir), 0.0);
+      vec3 diffuse = diff * diffuseLightColor;
+
+      // Feher szin atadasa
+      outColor = vec4((ambient + diffuse) * objectColor, 1.0);
     }
+    else
+    {
+      outColor = vec4(ambient * objectColor, 1.0);
+    }
+  }
 }
